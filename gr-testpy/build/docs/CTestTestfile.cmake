a900cbe04cbb5d183ec6a7c6c11cfc666a07cc1b
Jason Irby <irbyjm@gmail.com>
2017-10-07 16:00:05 -0400

    Delay testing, artifacts, notes

--- gr-testpy/build/docs/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/irbyjm/src/gr-transceive/gr-testpy/docs
-# Build directory: /home/irbyjm/src/gr-transceive/gr-testpy/build/docs
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(doxygen)
--- gr-testpy/build/swig/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/irbyjm/src/gr-transceive/gr-testpy/swig
-# Build directory: /home/irbyjm/src/gr-transceive/gr-testpy/build/swig
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
