33fc6e11cd871fb2bb0d0703db5c738eaf55e936
irbanoz <irbyjm@gmail.com>
2017-11-25 22:35:22 -0500

    Script works!

--- fix_decode.py
+++ fix_decode.py
@@ -4,15 +4,15 @@ import sys
 import binascii
 
 readfile = open(sys.argv[1], "rb")
-outfile = open("outfile", "wb")
+outfile = open(sys.argv[1]+".fixed", "wb")
 
 hexin = readfile.read()
-binary_string = bin(int(binascii.hexlify(hexin), 16))#[2:].zfill(8)
+binary_string = bin(int(binascii.hexlify(hexin), 16))[2:].zfill(8)
 
 for line in binary_string.split('01111110'):
     if len(line) % 8 == 0:
-        outfile.write(hex(int('0b'+line), 2))
-# output file should have line by line of "good decodes" or at least full-length 8-bit modulo decodes
+        newline = int('0b'+line, 2)
+        outfile.write(binascii.unhexlify('%x' % newline))
 
 readfile.close()
 outfile.close()
